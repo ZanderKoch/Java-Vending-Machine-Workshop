0c84fd31e7d399303143cf7c1f633b450d1a00bd
ZanderKoch <zander.koch02@gmail.com>
2025-02-05 12:01:23 +0100

    Add abstract class Product

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,32 @@
+package model;
+
+
+
+abstract class Product {
+    private static int idCounter = 0;
+    private final int id;
+    private double price;
+    private String productName;
+
+
+    public Product(String productName, double price){
+        this.productName = productName;
+        this.price = price;
+        this.id = ++idCounter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    abstract String use();
+    abstract String examine();
+}
